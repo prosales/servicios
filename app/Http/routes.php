5de8c45f408a9d7342a34cd922b6882c69cbbb03
Pablo Rosales <phablore@MacBook-Pro-de-Pablo.local>
2016-11-15 11:54:16 -0600

    Completo

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,10 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-header("Access-Control-Allow-Origin: *");
+/*header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Methods : GET,POST,PUT,DELETE,OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");*/
 
 Route::get('/', function () {
     return Redirect::to("src");
